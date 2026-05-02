9d07bc4db9cfcd7774cecec10af3455b743ce771
Sahil <56625327+sahillsa@users.noreply.github.com>
2026-05-02 12:56:05 +0530

    Add Angular bootstrap

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules))
+  ]
+}).catch((error) => console.error(error));
