50a02a91258bf62704d5c73b7c1ba66aa3bf5794
Sahil <56625327+sahillsa@users.noreply.github.com>
2026-05-02 12:55:44 +0530

    Add Capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.carehub.hospital',
+  appName: 'CareHub Hospital',
+  webDir: 'www'
+};
+
+export default config;
